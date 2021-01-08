72e874172e5f00fa9697dbfdfbaf8b5d50c3a322
Felipe Martins <felipemjesus@outlook.com>
2021-01-08 20:29:20 -0300

    Migrations and models created

---
+++ database/migrations/2021_01_08_184928_create_people_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('document_number', 14);
+            $table->string('cellphone', 11);
+            $table->string('email', 150);
+            $table->string('address', 150);
+            $table->string('number', 10);
+            $table->string('complement', 150)->nullable();
+            $table->string('neighborhood', 100)->nullable();
+            $table->string('zip_code', 8);
+            $table->tinyInteger('terms_accepted');
+            $table->tinyInteger('is_active')->default(false);
+            $table->foreignId('city_id')->constrained();
+            $table->foreignId('profile_id')->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('persons');
+    }
+}
