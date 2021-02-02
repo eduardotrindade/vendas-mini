9b50565737e23974d9278f682acd270681a0c390
Felipe Martins <felipemjesus@outlook.com>
2021-02-02 11:51:55 -0300

    Refactoring when importing components on the route

--- src/layouts/Admin.vue
+++ src/layouts/Admin.vue
@@ -22,23 +22,6 @@ export default {
 }
 </script>
 
-<style scoped>
-.bd-placeholder-img {
-  font-size: 1.125rem;
-  text-anchor: middle;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-}
-
-@media (min-width: 768px) {
-  .bd-placeholder-img-lg {
-    font-size: 3.5rem;
-  }
-}
-</style>
-
 <style>
 @import '../assets/css/admin/style.css';
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,20 +3,6 @@ import VueRouter from 'vue-router'
 import store from '@/store'
 
 import Site from '@/layouts/Site'
-import Admin from '@/layouts/Admin'
-
-import HomePage from '@/views/HomePage'
-import Products from '@/views/Products'
-import FinalizeOrder from '@/views/FinalizeOrder'
-import PaymentFeedback from "@/views/PaymentFeedback";
-import RegistrationPeople from '@/views/RegistrationPeople'
-import NotFound from '@/views/NotFound'
-
-import Login from '@/views/admin/Login'
-import UsersList from '@/views/admin/users/UsersList'
-import PeopleList from '@/views/admin/people/PeopleList'
-import PeopleView from '@/views/admin/people/PeopleView'
-import OrdersList from '@/views/admin/orders/OrdersList'
 
 Vue.use(VueRouter)
 
@@ -24,70 +10,65 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomePage,
+    component: () => import(/* webpackChunkName: "site" */ '@/views/HomePage'),
     meta: { layout: Site },
   },
   {
     path: '/produtos',
     name: 'products',
-    component: Products,
+    component: () => import(/* webpackChunkName: "site" */ '@/views/Products'),
     meta: { layout: Site },
   },
   {
     path: '/finalizar-compra',
     name: 'finalize-order',
-    component: FinalizeOrder,
+    component: () => import(/* webpackChunkName: "site" */ '@/views/FinalizeOrder'),
     meta: { layout: Site },
   },
   {
     path: '/feedback',
     name: 'feedback',
-    component: PaymentFeedback,
+    component: () => import(/* webpackChunkName: "site" */ '@/views/PaymentFeedback'),
     meta: { layout: Site },
   },
   {
     path: '/seja-nosso-representante',
     name: 'registration-people',
-    component: RegistrationPeople,
+    component: () => import(/* webpackChunkName: "site" */ '@/views/RegistrationPeople'),
     meta: { layout: Site },
   },
   {
     path: '/login',
     name: 'login',
-    component: Login,
+    component: () => import(/* webpackChunkName: "login" */ '@/views/admin/Login'),
     meta: { layout: Site },
   },
   {
     path: '/admin',
     name: 'admin',
-    component: Admin,
+    component: () => import(/* webpackChunkName: "admin" */ '@/layouts/Admin'),
     meta: { requiresAuth: true },
     children: [
       {
-        path: 'users',
-        name: 'users-list',
-        component: UsersList
+        path: 'orders',
+        name: 'orders-list',
+        component: () => import(/* webpackChunkName: "orders" */ '@/views/admin/orders/OrdersList'),
       },
       {
         path: 'people',
         name: 'people-list',
-        component: PeopleList
+        component: () => import(/* webpackChunkName: "people" */ '@/views/admin/people/PeopleList'),
       },
       {
         path: 'people/:id',
         name: 'people-view',
-        component: PeopleView
-      },
-      {
-        path: 'orders',
-        name: 'orders-list',
-        component: OrdersList
+        component: () => import(/* webpackChunkName: "people" */ '@/views/admin/people/PeopleView'),
       },
     ]
   },
   {
     path: "*",
-    component: NotFound,
+    component: () => import(/* webpackChunkName: "not-found" */ '@/views/NotFound'),
   },
 ]
 
