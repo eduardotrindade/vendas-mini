92cc484a7699b08acba91e8bf1af63c900d77bfd
Felipe Martins <felipemjesus@outlook.com>
2021-02-02 20:52:04 -0300

    Redirecting any web route to the front-end

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+// Redirecting any web route to the front-end
+Route::get('/{vue_capture?}', function () {
     return File::get(public_path('index.html'));
-});
+})->where('vue_capture', '[@\/\w\.\-\+]*');
