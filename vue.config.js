c12f1ae83f6cd4a23fe26b3c3af0eac1544a4cc5
Felipe Martins <felipemjesus@outlook.com>
2021-02-02 20:35:23 -0300

    Update workflow homologation

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  productionSourceMap: false,
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
+      }
+    },
+  },
+  css: {
+    extract: true
+  },
+  publicPath: process.env.VUE_APP_PUBLIC_PATH,
+  outputDir: process.env.VUE_APP_OUTPUT_DIR,
+  assetsDir: process.env.VUE_APP_ASSETS_DIR,
+}
